d6ce5cbb9ebe1b37b1e3335fdc4786137bbad997
Eugene Lysov <evg.lysov@gmail.com>
2020-08-20 21:37:36 +0300

    готово ДЗ 1

--- js/task_2.js
+++ js/task_2.js
@@ -1,13 +1,13 @@
-const invoice = 100;
-const stock = 100;
+// const invoice = 100;
+// const stock = 100;
 
-// Write code under this line
-const message =
-  invoice <= stock
-    ? 'Заказ оформлен, с вами свяжется менеджер'
-    : 'На складе недостаточно товаров!';
+// // Write code under this line
+// const message =
+//   invoice <= stock
+//     ? 'Заказ оформлен, с вами свяжется менеджер'
+//     : 'На складе недостаточно товаров!';
 
-console.log(message);
+// console.log(message);
 
 //если invoice равен 100
 // то значение message будет равно
@@ -20,3 +20,41 @@ console.log(message);
 //если invoice равен 150
 // то значение message будет равно
 // 'На складе недостаточно товаров!'
+const countryName = 'Китай';
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const NO_DELIVERY = 'В выбранную страну доставка недоступна.';
+const CHINA = 'Китай';
+const AUSTRALIA = 'Австралия';
+const INDIA = 'Индия';
+const JAMAICA = 'Ямайка';
+let message;
+let price = 0;
+let country;
+if (countryName === null) {
+  message = CANCELED_BY_USER;
+} else {
+  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase; // Write code on this line
+  switch (country) {
+    // Write code under this line
+    case CHINA:
+      price = 100;
+      break;
+    case AUSTRALIA:
+      price = 170;
+      break;
+    case INDIA:
+      price = 80;
+      break;
+    case JAMAICA:
+      price = 120;
+      break;
+    default:
+      price >= 0;
+      message = NO_DELIVERY;
+  }
+}
+if (price >= 0) {
+  // Write code on this line
+  message = `Доставка в ${country} будет стоить ${price} кредитов`;
+}
+console.log(message);
